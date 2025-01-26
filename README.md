977a217fa9e4848826f6ac32b09daf9ddd3f5f72
Changin <qorckddls010@gmail.com>
2025-01-26 21:55:53 +0900

    백창인 - 패키지 추가

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # README
 
 ## DO NOT TOUCH THE MASTER BRANCH!!
+
+- 세종대학교 24-동계 창의학기제
+- 동일 무선 채널을 활용한 딥러닝 기반 다중 RF 신호 검출 및 복조 기술 개발
--- rnn.py
+++ rnn.py
@@ -1,12 +1,18 @@
 # rnn.py
+# 패키지 필요 시 추가 & 공유하기
+from torch.utils.data import TensorDataset, DataLoader
+import torch
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-#
+# 데이터 불러오기 - 규태
 
-#
+# 데이터 전처리 - 수현
 
-#
+# 모델 정의 - 창인
 
-#
+# 학습 - 채원
 
-#
+# 그래프 출력 & 성능 평가 - 민지
