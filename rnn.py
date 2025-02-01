b5f1cab6f6ac09fb6a04a74ba71f63ba6d7fefc4
Changin <qorckddls010@gmail.com>
2025-02-01 20:25:23 +0900

    FIX - 백창인 : 데이터 불러오기 수정

--- rnn.py
+++ rnn.py
@@ -8,7 +8,16 @@ import numpy as np
 
 
 # *************** 데이터 불러오기 - 규태 ***************
+train_x_file = 'C:/pdpd/data/train_x_bit_QPSK_L3_dec_fin_save.csv'
+test_x_file = 'C:/pdpd/data/test_x_bit_QPSK_L3_dec_fin_save.csv'
+train_x_r1_file = 'C:/pdpd/data/train_x_bit_QPSK_L3_2X2_save.csv'
+test_x_r1_file = 'C:/pdpd/data/test_x_bit_QPSK_L3_2X2_save.csv'
 
+train_Y_file = 'C:/pdpd/data/train_Y_QPSK_L3_ReIm_2X2_save.csv'
+test_Y_file = 'C:/pdpd/data/test_Y_QPSK_L3_ReIm_2X2_save.csv'
+
+snr_list = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24]
+snr_to_col = {snr: i for i, snr in enumerate(snr_list)}
 
 # *************** 데이터 전처리 - 수현 ****************
 
